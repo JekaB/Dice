69aab95bab141d9e9cc7461dbb7f7ae54d2a190f
Evgeiy_B <bocharovevgen@gmail.com>
2015-07-13 16:53:43 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qt;
+            string ans = "";
+            Console.Write("Roll-the-dice ");
+            qt = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Rolling {0} dice...", qt);
+            float prob = 1;
+            Random rnd = new Random();
+            for (int i = 0; i < qt; i++)
+            {                
+                float dice = rnd.Next(1, 13);
+                ans += dice + " ";
+                
+                prob *= (dice / 12);
+            }
+
+            Console.WriteLine("{0} (Probability: {1:0.000})", ans, prob);
+
+            Console.ReadKey();
+        }
+    }
+}
